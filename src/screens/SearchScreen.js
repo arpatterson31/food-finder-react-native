475a09dcb788ad96556ef97c581591151f9390d8
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-08-03 14:42:08 -0700

    useeffect to hit api on component render

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
@@ -8,12 +8,12 @@ const SearchScreen = () => {
   const [results, setResults] = useState([]);
   const [errorMessage, setErrorMessage] = useState('');
 
-  const searchAPI = async () => {
+  const searchAPI = async searchTerm => {
     try {
       const response = await yelp.get('/search', {
         params: {
           limit: 50,
-          term: term,
+          term: searchTerm,
           location: 'Seattle'
         }
       });
@@ -23,12 +23,16 @@ const SearchScreen = () => {
     }
   };
 
+  useEffect(() => {
+    searchAPI('pasta');
+  },[])
+
   return (
     <View>
       <SearchBar 
         term={term} 
         onTermChange={setTerm}
-        onTermSubmit={searchAPI}
+        onTermSubmit={() => searchAPI(term)}
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>

08e31cd6bbee196d1d12462eb2b456a2290a9db4
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-08-03 13:05:08 -0700

    bringing in font awesome for search icon

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
 
 const SearchBar = () => {
   return (
     <View style={styles.background}>
+      <FontAwesome name="search" size={30} />
       <Text>Search Bar</Text>
     </View>
   );

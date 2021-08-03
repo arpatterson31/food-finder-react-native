2e31078700cb898ac2c46c6c67cac510300292ee
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-08-03 14:10:09 -0700

    added function to make api call

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,18 +1,36 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
+  const [results, setResults] = useState([]);
+
+  const searchAPI = async () => {
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term: term,
+          location: 'Seattle'
+        }
+      });
+      setResults(response.data.businesses);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   return (
     <View>
       <SearchBar 
         term={term} 
-        onTermChange={(newTerm) => setTerm(newTerm)}
-        onTermSubmit={() => console.log('term was submitted')}
+        onTermChange={setTerm}
+        onTermSubmit={searchAPI}
       />
       <Text>Search Screen</Text>
+      <Text>We have found {results.length} results</Text>
     </View>
   );
 };

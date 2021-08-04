8f0015761db8b1a30c013925c68f55bc729a6651
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-08-03 21:32:48 -0700

    set state for restaurant info from yelp call and added a useEffect call to set it to state

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import yelp from '../api/yelp';
 
 const ResultsShowScreen = ({ navigation }) => {
+  const [result, setResult] = useState(null);
   const id = navigation.getParam('id');
-  
+
+  const getRestaurant = async (id) => {
+    await yelp.get(`/${id}`);
+    setResult(response.data);
+  };
+
+  useEffect(() => {
+    getResult(id);
+  }, []);
+
   return (
     <View>
       <Text>Results Show Screen</Text>
